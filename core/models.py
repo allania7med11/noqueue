421c32cf6aef7293df7793b2512e9466f467edfe
allania7med11 <allania7med11@gmail.com>
2023-11-26 04:00:58 +0100

    add Queue model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User 
 
-# Create your models here.
+
+class Queue(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+class QueueCitation(models.Model):
+    queue = models.ForeignKey(Queue, on_delete=models.CASCADE)
+    number = models.IntegerField()
+    state_choices = [
+        ('NS', 'Not Served'),
+        ('SV', 'Served'),
+    ]
+    state = models.CharField(max_length=2, choices=state_choices)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.queue} - {self.number} ({self.get_state_display()})"
